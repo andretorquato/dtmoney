9cf752ba69c0716ab0da2437a17df16173310ba5
André Torquato <ats.torquato1@gmail.com>
2021-03-23 23:55:40 -0300

    :sparkles:Componentização

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+
 import { GlobalStyle } from './styles/global';
 
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header />
+      <Dashboard/>
       <GlobalStyle />
-    </div>
+    </>
   );
 }
 
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionsTable(){
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Categorias</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td className="deposit">R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td className="withdraw">-R$1.200</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+              
+                </tbody>
+            </table>
+        </Container>
+    );
+}
